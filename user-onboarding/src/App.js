20ce9c0277f0000ad4c28adf5a71755429d2465b
Leelsmuth <femianjorin@rocketmail.com>
2019-09-19 12:45:24 +0100

    initial commit

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+// import axios from "axios";
+import UserForm from "./Form";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User Onboarding</h1>
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
