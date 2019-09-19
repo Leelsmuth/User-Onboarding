0198c2d0fc0143aa0624f580ad06f34dc1d53c2a
Leelsmuth <femianjorin@rocketmail.com>
2019-09-19 14:23:25 +0100

    wip

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,15 +1,74 @@
 import React, { useState, useEffect } from "react";
-// import axios from "axios";
+import axios from "axios";
+import * as yup from "yup";
 import UserForm from "./Form";
 import "./App.css";
 
+const formApi = "https://reqres.in/api/users";
+
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const [serverError, setServerError] = useState("");
+
+  const fetchUsers = () => {
+    // get those friends from the api
+    // and set them into the right slice of state!
+    // don't forget about the sad path: put something in `serverError`
+    axios
+      .get(formApi)
+      .then(res => {
+        setUsersList(res.data);
+      })
+      .catch(err => {
+        setServerError(err.message);
+      });
+  };
+
+  const addUser = (formValues, actions) => {
+    // THIS FUNCTION NEEDS TO COMPLY WITH FORMIK
+    // REQUIREMENTS FOR ACCEPTABLE `onSubmit` FUNCTIONS!
+    // It should take two args:
+    //     (values) the form values (object)
+    //     (actions) formik actions (object)
+    // And perform a POST request to the api
+    const UserToPost = {
+      name: formValues.name,
+      email: formValues.email,
+      password: formValues.password
+    };
+    axios
+      .post(formApi, UserToPost)
+      .then(res => {
+        // res.data contains the newly created friend
+        const newlyCreatedUser = res.data;
+        setUsersList(usersList.concat(newlyCreatedUser));
+        actions.resetForm();
+      })
+      .catch(err => {
+        debugger;
+      });
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div className="App">
       <h1>User Onboarding</h1>
-      <UserForm />
+      <UserForm onSubmit={addUser} />
+
+      {usersList.length
+        ? usersList.map(fr => <div key={fr}>{fr.name} is Saved.</div>)
+        : "No Users. Sad!"}
     </div>
   );
 }
+
+const validationSchema = yup.object().shape({
+  name: yup.string().required("GAGAHHH WE NEED NAME"),
+  email: yup.email().required(),
+  password: yup.password().required()
+});
 
 export default App;
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,8 +1,58 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 
-function UserForm() {
-  return console.log("item");
+const InitialOnboardingForm = {
+  name: "",
+  email: "",
+  password: ""
+};
+
+function UserForm({ onSubmit }) {
+  return (
+    <Formik
+      // validationSchema={validationSchema}
+      // validate={validate}
+      initialValues={InitialOnboardingForm}
+      onSubmit={onSubmit}
+      render={props => {
+        return (
+          // we will use pre-baked components
+          // supplied by formik lib (like Formik)
+          <Form>
+            <div>
+              <label>
+                Name
+                <Field name="name" type="text" placeholder="Name" />
+                <ErrorMessage name="name" component="div" />
+              </label>
+            </div>
+            <div>
+              <label>
+                Email
+                <Field name="email" type="email" placeholder="Enter Email" />
+                <ErrorMessage name="email" component="div" />
+              </label>
+            </div>
+            <div>
+              <label>
+                password
+                <Field name="password" type="password" placeholder="password" />
+                <ErrorMessage name="password" component="div" />
+              </label>
+            </div>
+            <div>
+              <label>
+                Terms of Service
+                <Field name="terms" type="checkbox" placeholder="Age" />
+                <ErrorMessage name="terms" component="div" />
+              </label>
+            </div>
+            <button type="submit">Submit</button>
+          </Form>
+        );
+      }}
+    />
+  );
 }
 
 export default UserForm;
